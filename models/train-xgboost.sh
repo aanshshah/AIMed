ac2718997011994e05a38ce2ccc209a929a1e24b
Aansh Shah <aansh.shah@gmail.com>
2019-02-26 15:37:27 -0500

    added xgboost stuff

---
+++ models/train-xgboost.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Request an hour of runtime:
+#SBATCH --time=00:60:00
+
+# Ask for the GPU partition and 1 GPU
+#SBATCH -p gpu --gres=gpu:1
+
+# 4 cores
+#SBATCH -n 4
+
+# 20G
+#SBATCH --mem=20G
+
+# Specify a job name:
+#SBATCH -J XGBoostTrain
+
+# Specify an output file
+#SBATCH -o XGBoost.out
+#SBATCH -e XGBoost.out
+
+
+# Set up the environment by loading modules
+module load anaconda/3-5.2.0
+module load cuda/8.0.61 cudnn/5.1
+source activate /users/ashah3/scratch/env
+
+# Run a script
+python xg_boost_fit.py
