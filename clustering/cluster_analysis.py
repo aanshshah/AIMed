7ae062de96b8db7e19e0f90eb0602b4c0bada633
jramos70 <jerome_ramos@brown.edu>
2019-03-05 13:32:47 -0500

    added descriptions to cluster count graphs

--- clustering/cluster_analysis.py
+++ clustering/cluster_analysis.py
@@ -4,10 +4,18 @@ import matplotlib.pyplot as plt
 from collections import Counter
 from scipy.misc import comb
 
-def generate_plot(groupss):
+def generate_plot(groups, labels):
+    '''
+        Generates CCS count bar graphs for each cluster
+
+        Args:
+            groups: a 2D array of (clusters, CCS codes)
+            labels: a dictionary mapping CCS codes to descriptions
+        Returns:
+            Saves CCS bar graphs inside graphs folder
+    '''
     index = 1
     for group in groups:
-        print ("Group size", len(group))
         ccs_count = {}
         for codes in group:
             cleaned_codes = codes.strip("()").split(", ")
@@ -19,22 +27,23 @@ def generate_plot(groupss):
                     ccs_count[code] = 1
         # print(ccs_count)
         ccs_count = Counter(ccs_count)
-        common = ccs_count.most_common()[:15]
+        common = ccs_count.most_common()[:10]   #10 most common CCS codes
         x, y = [], []
         for i,j in common:
-            x.append(i)
+            x.append(labels[i])
             y.append(j)
+        
+        #Formats labels
+        fig, ax = plt.subplots()
+        fig.autofmt_xdate()
 
-        print(x)
-        print(y)
         x_pos = [i for i, _ in enumerate(x)]
         plt.bar(x_pos, y, color='blue')
         plt.xlabel("CCS")
         plt.ylabel("Number of Patients")
         plt.title("CCS Counts for Group " + str(index))
-        # plt.ylim(min(y), max(y))
         plt.xticks(x_pos, x)
-        plt.savefig('graphs/SOM_CCS_cluster' + str(index) +'.png', bbox_inches='tight')
+        plt.savefig('graphs/Kmodes_CCS_cluster' + str(index) +'.png', bbox_inches='tight')
         index += 1
         plt.clf()
         # plt.show()
@@ -43,10 +52,11 @@ def calc_similarity(groups):
     """Calculates the similarity score of each cluster by getting the sum of the set difference
 
     Args:
-        param1 (groups): A list of the dictionaries of clusters (key: CCS code, value: # patients)
+        groups: A list of the dictionaries of clusters (key: CCS code, value: # patients)
 
     Returns:
-        Similarity score. The higher the number, the better
+        score: Similarity score. The higher the number, the better
+        cnt_overlap_key: cluster overlap. Counts number of overlapping keys in cluster. The lower, the better
 
     """
     clusters = [] 
@@ -78,15 +88,32 @@ def calc_similarity(groups):
                     cnt_overlap_key += 1
     return score, cnt_overlap_key
     
-#[1,2,3]
-#[1,4]
+def get_labels(df):
+    '''
+        Maps a dictionary of icd codes to disease
+
+        Args:
+            df: ICD dataframe
+        Returns: 
+            dictionary (code to disease)
+    '''
+    df['CCS_clean_category'] = df["\'CCS CATEGORY\'"].str.strip(' "\'\t\r\n')
+    df['clean_desc'] = df["\'CCS CATEGORY DESCRIPTION\'"].str.strip(' "\'\t\r\n')
+    return dict(zip(df["CCS_clean_category"], df['clean_desc']))
+
+
 if __name__ == "__main__":
-    data = pd.read_csv("../data/patient_ccs_som_30x30.csv")
-    # data = pd.read_csv("../data/patient_ccs_100.csv")
+    # data = pd.read_csv("../data/patient_ccs_som_30x30.csv")
+    icd = pd.read_csv("../data/CCS.csv", skiprows=[0,2])
+    labels = get_labels(icd)
+    data = pd.read_csv("../data/patient_ccs_100.csv")
+
+    #generate graphs
     groups = data.groupby('cluster_num')['CCS_codes'].apply(list)
-    for i in groups:
-        print(len(i))
+    generate_plot(groups, labels)
+
+
+    #similarity scores
     # score, cnt = calc_similarity(groups)
     # print("Similarity Score:", score)
     # print("Average # of overlapping CCS codes:", cnt/comb(len(groups), 2))
-    # generate_plot(groups)
